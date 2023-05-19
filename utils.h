2d7a2af3b3e647756b3607933602b82183505521
Natasha Chiorsac <nchiorsac@student.unimelb.edu.au>
2023-05-19 10:46:27 +0000

    Delete utils.h

--- utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-int setup_server_socket(int port, struct sockaddr_in* address);
-int setup_client_socket(const int port, struct sockaddr_in* server_address);
-
-#endif
